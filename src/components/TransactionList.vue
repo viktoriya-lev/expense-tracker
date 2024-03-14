d8620b35ec6f624552fa16a62791813fb38e57df
Tori <toriya.levitskaya@mail.ru>
2024-03-14 22:35:23 +0300

    main components created; some basic settings made

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">
+      Cash <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Paycheck <span>$1500</span><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+
+</script>
